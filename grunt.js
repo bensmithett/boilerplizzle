7e7253ca435120b8699fb4bb7ba749a310e45422
Ben Smithett <bsmithett@gmail.com>
2012-10-16 17:42:38 +1100

    Add gruntfile

---
+++ grunt.js
@@ -0,0 +1,46 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: '<json:package.json>',
+    meta: {
+      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+    },
+    concat: {
+      dist: {
+        src: ['<banner:meta.banner>', '<file_strip_banner:lib/<%= pkg.name %>.js>'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    min: {
+      dist: {
+        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+    watch: {
+      files: ['<config:coffee.app.src>'],
+      tasks: 'coffee'
+    },
+    uglify: {},
+
+    coffee: {
+      app: {
+        src: ['coffee/**/*.coffee'],
+        dest: 'build/js'
+      }
+    },
+  });
+
+  // Load npm tasks
+  grunt.loadNpmTasks('grunt-coffee');
+
+  // Default task.
+  grunt.registerTask('default', 'lint qunit concat min');
+
+};
