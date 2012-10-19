544e9ef76dcdad06b8a4c8d1d5019e80cc029a7c
Ben Smithett <bsmithett@gmail.com>
2012-10-19 23:15:58 +1100

    Add install.sh that wgets some remote JS libs

---
+++ install.sh
@@ -0,0 +1,5 @@
+# Get JS libs
+mkdir -p build/js/lib
+wget -O build/js/lib/jquery.min.js http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js
+wget -O build/js/lib/lodash.min.js http://cdnjs.cloudflare.com/ajax/libs/lodash.js/0.8.2/lodash.min.js
+wget -O build/js/lib/modernizr.min.js http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js
