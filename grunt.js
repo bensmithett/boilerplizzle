d4d2be288b38547050e24a55d96379ef2c16401a
Ben Smithett <bsmithett@gmail.com>
2012-10-21 21:43:15 +1100

    Delete unnecessary stuff from gruntfile & add npm install step to install script.

--- grunt.js
+++ grunt.js
@@ -4,18 +4,7 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: '<json:package.json>',
-    meta: {
-      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-        '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
-        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
-    },
     concat: {
-      dist: {
-        src: ['<banner:meta.banner>', '<file_strip_banner:lib/<%= pkg.name %>.js>'],
-        dest: 'dist/<%= pkg.name %>.js'
-      },
       head: {
         src: ['build/js/lib/modernizr.min.js', 'build/js/load.js'],
         dest: 'build/js/head.js'
--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ mkdir -p build/js/lib
 wget -O build/js/lib/jquery.min.js http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js
 wget -O build/js/lib/lodash.min.js http://cdnjs.cloudflare.com/ajax/libs/lodash.js/0.8.2/lodash.min.js
 wget -O build/js/lib/modernizr.min.js http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js
+npm install
